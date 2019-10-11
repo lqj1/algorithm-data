d54753a5149c0a3fc32e9920f6e7070874e42b6a
lqj1 <1104942848@qq.com>
2019-10-11 20:21:29 +0800

    test vscode

---
+++ 基础算法/冒泡排序.py
@@ -0,0 +1,10 @@
+def bubble_sort(arr):
+    for i in range(len(arr)-1):
+        
+
+
+if __name__ == '__main__':
+    data = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    print(data)
+    bubble_sort(data)
+    print(data)
