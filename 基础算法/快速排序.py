b4686365888a9320aef99aa340fd3e82903a9a5e
lqj1 <1104942848@qq.com>
2019-10-14 17:00:35 +0800

    add new files

---
+++ 基础算法/快速排序.py
@@ -0,0 +1,8 @@
+def quick_sort(nums, left, right):
+    
+if __name__ == '__main__':
+    data = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    print(data)
+    lens = len(data)
+    quick_sort(data, 0, lens-1)
+    print(data)
