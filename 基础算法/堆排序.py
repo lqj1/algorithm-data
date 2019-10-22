4757a0c2a4f2aefaf2b96d6a7391c7607376e14c
lqj1 <1104942848@qq.com>
2019-10-22 19:12:58 +0800

    add new files

--- 基础算法/堆排序.py
+++ 基础算法/堆排序.py
@@ -1,17 +1,28 @@
 
-def heapify(arr, n, i):
-    
-
-
+def heapify(arr, n, i):  #构建最大堆，从非叶子节点开始判断
+    largest = i
+    l = 2*i + 1  #left
+    r = 2*i + 2  #right
+    if l < n and arr[largest]<arr[l]:   #堆的左右节点位置可以不一样
+        largest = l
+    if r < n and arr[largest]<arr[r]:
+        largest = r
+    if largest != i:
+        arr[i],arr[largest] = arr[largest],arr[i]
+        heapify(arr,n,largest)  #每次调整完后需要从最后的该节点继续往前面判断
 
 def heapSort(arr):
     length = len(arr)
     # 构建最大堆
     for i in range(length,-1,-1):
-        heapify(arr,n,i)
+        heapify(arr,length,i)
+    # 构建完之后就可以得到初始堆
+    for i in range(length-1,0,-1):  #只需要排序到下标为1的数
+        arr[0],arr[i] = arr[i],arr[0]  #此时的堆顶元素是最大的，将堆顶元素与最后一个元素交换
+        heapify(arr,i,0)
 
 if __name__ == '__main__':
-    data = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    data = [16,7,3,20,17,8]
     print(data)
     heapSort(data)
     print(data)
