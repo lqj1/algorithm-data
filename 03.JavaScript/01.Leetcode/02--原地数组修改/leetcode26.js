58d2844c53bfb25b212f33d095f65ab09385942e
lqj1 <1104942848@qq.com>
2022-01-24 17:52:21 +0800

    Update 新增leetcode26

---
+++ 03.JavaScript/01.Leetcode/02--原地数组修改/leetcode26.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+//  示例 1：
+
+//  输入：nums = [1,1,2]
+//  输出：2, nums = [1,2]
+//  解释：函数应该返回新的长度 2 ，并且原数组 nums 的前两个元素被修改为 1, 2 。不需要考虑数组中超出新长度后面的元素。
+
+var removeDuplicates = function (nums) {
+  // 定义快慢指针
+  let slow,
+    fast = 0;
+  for (let index = 0; index < nums.length; index++) {
+    const element = nums[index];
+    
+  }
+};
