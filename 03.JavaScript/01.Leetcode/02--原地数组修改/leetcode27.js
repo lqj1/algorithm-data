7661796b5bc98ef68a1db7b27fc24628b8b5ee7c
lqj1 <1104942848@qq.com>
2022-01-26 09:33:41 +0800

    Update 完成leetcode27

--- 03.JavaScript/01.Leetcode/02--原地数组修改/leetcode27.js
+++ 03.JavaScript/01.Leetcode/02--原地数组修改/leetcode27.js
@@ -12,4 +12,20 @@
 // 输出：2, nums = [2,2]
 // 解释：函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。你不需要考虑数组中超出新长度后面的元素。例如，函数返回的新长度为 2 ，而 nums = [2,2,3,3] 或 nums = [2,2,0,0]，也会被视作正确答案。
 
-var removeElement = function (nums, val) {};
+// 原地修改，就可以考虑双指针技巧
+var removeElement = function (nums, val) {
+  let slow = 0,
+    fast = 0,
+    length = nums.length;
+  while (fast <= length - 1) {
+    if (nums[fast] === val) {
+      fast++;
+    } else {
+      nums[slow++] = nums[fast++];
+    }
+  }
+  return slow;
+};
+// [2, 3, 2, 3];
+// slow;
+//    fast;
