0596707f180d4bb4d9503405b56dd0321f5966c2
lqj1 <1104942848@qq.com>
2022-03-16 14:23:57 +0800

    feat: solve leetcode 21

--- 03.JavaScript/01.Leetcode/21.合并两个有序链表.js
+++ 03.JavaScript/01.Leetcode/21.合并两个有序链表.js
@@ -17,29 +17,37 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
+// 迭代法
+// var mergeTwoLists = function (list1, list2) {
+//   // 定义虚拟的头节点，就不需要判断并指向list1或list2小的第一个节点
+//   let dummy = new ListNode(-1),
+//     forward = dummy // 引用类型赋值共享内存空间
+//   while (list1 && list2) {
+//     if (list1.val <= list2.val) {
+//       forward.next = list1
+//       list1 = list1.next
+//     } else {
+//       forward.next = list2
+//       list2 = list2.next
+//     }
+//     forward = forward.next
+//   }
+//   forward.next = list1 === null ? list2 : list1
+//   return dummy.next
+// }
+
+// 递归法
 var mergeTwoLists = function (list1, list2) {
-  let p = null,
-    temp = null
-  if (list1.val <= list2.val) {
-    p = list1
-    list1 = list1.next
+  // 定义虚拟的头节点，就不需要判断并指向list1或list2小的第一个节点
+  if (!list1) return list2
+  else if (!list2) {
+    return list1
+  } else if (list1.val <= list2.val) {
+    list1.next = mergeTwoLists(list1.next, list2)
+    return list1
   } else {
-    p = list2
-    list2 = list2.next
+    list2.next = mergeTwoLists(list1, list2.next)
+    return list2
   }
-  while (list1 && list2) {
-    if (list1.val <= list2.val) {
-      temp = list1
-      list1 = list1.next
-      p.next = temp
-    } else {
-      temp = list2
-      list2 = list2.next
-      p.next = temp
-    }
-    console.log(p)
-  }
-  p.next = list1 ? list1 : list2
-  return p
 }
 // @lc code=end
